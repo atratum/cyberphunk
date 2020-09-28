28e099d93af572cc4e6526b34b840ef20407a889
Jordan Warren <atratum@protonmail.com>
2020-09-28 10:02:53 +0100

    Amend logo casing, spacing, and alignment

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
         <div class="container"></div>
       </header>
       <main>
-        <h1 class="logo">Cyberphunk</h1>
-        <div class="tagline">Bespoke cyber solutions</div>
+        <div class="container text-center">
+          <h1 class="logo">Cyberphunk</h1>
+          <div class="tagline">Bespoke cyber solutions</div>
+        </div>
       </main>
       <footer>
         <div class="container"></div>
